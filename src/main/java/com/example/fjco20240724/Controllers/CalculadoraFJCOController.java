d878f0cb408d723aa4302bc1b87a12e5ab208844
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-07-24 10:26:47 -0600

    Se agrego funcionalidad en controlador y pagina falta diseño

--- src/main/java/com/example/fjco20240724/Controllers/CalculadoraFJCOController.java
+++ src/main/java/com/example/fjco20240724/Controllers/CalculadoraFJCOController.java
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/calculadora")
 public class CalculadoraFJCOController {
-    @GetMapping("/suma")
+    @GetMapping("/resta")
     public String showSumForm(Model model) {
         model.addAttribute("num1", 0);
         model.addAttribute("num2", 0);
         model.addAttribute("result", 0);
-        return "Calculadora/suma";
+        return "CalculadoraFJCO/restafjco";
     }
 
-    @PostMapping("/suma")
+    @PostMapping("/resta")
     public String performSum(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
-        int result = num1 + num2;
+        int result = num1 - num2;
         model.addAttribute("num1", num1);
         model.addAttribute("num2", num2);
         model.addAttribute("result", result);
-        return "calculadora/suma";
+        return "CalculadoraFJCO/restafjco";
     }
 }
