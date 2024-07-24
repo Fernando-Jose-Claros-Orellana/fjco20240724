10536bd3fbac5410a650b4085669a5d9d6524df8
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-07-24 10:12:25 -0600

    -

--- src/main/java/com/example/fjco20240724/Controllers/CalculadoraFJCOController.java
+++ src/main/java/com/example/fjco20240724/Controllers/CalculadoraFJCOController.java
@@ -1,4 +1,29 @@
 package com.example.fjco20240724.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/calculadora")
 public class CalculadoraFJCOController {
+    @GetMapping("/suma")
+    public String showSumForm(Model model) {
+        model.addAttribute("num1", 0);
+        model.addAttribute("num2", 0);
+        model.addAttribute("result", 0);
+        return "Calculadora/suma";
+    }
+
+    @PostMapping("/suma")
+    public String performSum(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+        int result = num1 + num2;
+        model.addAttribute("num1", num1);
+        model.addAttribute("num2", num2);
+        model.addAttribute("result", result);
+        return "calculadora/suma";
+    }
 }
